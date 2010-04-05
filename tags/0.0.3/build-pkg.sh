ef682a3585e1996c70737aa72bd91aa5ed889f52
Bruce Duncan <bwduncan@gmail.com>
2010-04-05 14:00:09 +0100

    Un-SVNify repository (remove tags, move trunk to top level).

--- tags/0.0.3/build-pkg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VERSION=`head -n 1 debian/changelog | sed -e 's/^.* (\(.*\)).*/\1/'`
-if [ -d "../tags/$VERSION" ]; then
-  echo "Tag $VERSION already exists - do you need to update the changelog?"
-  exit
-fi
-
-svn-buildpackage --svn-tag --svn-noautodch -k660241D2
