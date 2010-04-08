d3637f1f393e102a896c7c45d30a53ca193a1a27
Matthew Richardson <M.Richardson@ed.ac.uk>
2010-04-08 16:55:29 +0100

    Added simple POD format man pages for certain and certain.cfg, Generate man pages as well in doc_make.sh

--- make_doc.sh
+++ make_doc.sh
@@ -2,3 +2,8 @@
 
 #$@ allows insertion of extra options, e.g --pdf
 epydoc -v --name Certain $@ certain
+
+#Make man pages
+pod2man man/certain.pod >man/certain.1
+pod2man man/certain.cfg.pod >man/certain.cfg.5
+
