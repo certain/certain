5a332f55cf495bd3d303082aed6726ed074d9595
Matthew Richardson <M.Richardson@ed.ac.uk>
2010-04-16 11:44:39 +0100

    Simple list-building example added.

--- certain/gui.py
+++ certain/gui.py
@@ -5,15 +5,35 @@ from . import CertainForm
 from . import DEFAULT_CONFIG_FILE
 import sys
 
+
 class MainWindow(QtGui.QMainWindow):
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.ui = CertainForm.Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.csrList.setColumnCount(2)
+        self.ui.csrList.setColumnWidth(0, 150)
         self.ui.csrList.setHorizontalHeaderLabels(QtCore.QStringList(
             ['Common Name', 'Action']))
+
+        rowcount = 0
+        colminwidth = 5
+        self.ui.csrList.setColumnWidth(0, colminwidth)
+        entries = ['a', 'b', 'long-named-cert']
+
+        self.ui.csrList.setRowCount(len(entries))
+        for entry in entries:
+            actionlist = QtGui.QComboBox()
+            actionlist.insertItems(0, ['', 'Sign', 'Delete'])
+            if len(entry) * 10 > colminwidth:
+                colminwidth = len(entry) * 10
+                self.ui.csrList.setColumnWidth(0, colminwidth)
+            self.ui.csrList.setCellWidget(
+                entries.index(entry), 0, QtGui.QLabel(entry))
+            self.ui.csrList.setCellWidget(
+                entries.index(entry), 1, actionlist)
 
         self._reset()
 
@@ -23,6 +43,7 @@ class MainWindow(QtGui.QMainWindow):
             self.ui.csrList.setItem(i, 1, csr.CN)
         #with open(DEFAULT_CONFIG_FILE) as f:
         #    self.ui.config.insertPlainText(f.read())
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
