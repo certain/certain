ea54bbe7e54d0605ed85007bef608cdc77e9d92d
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2010-04-20 17:14:07 +0100

    Move certain.cfg to setup/etc/certain for clarity.

--- setup.py
+++ setup.py
@@ -27,14 +27,13 @@ if sys.argv[1] == 'install':
     os.system('sphinx-build -b html sphinx ' + os.path.join('setup', 'sphinx'))
 
     try:
-        os.mkdir('setup')
-        os.mkdir('setup/etc')
+        os.makedirs('setup/etc/certain')
     except OSError, e:
         if e.errno != errno.EEXIST:
             raise
 
     #Copy certain.cfg.defaults to certain.cfg
-    with open('setup/etc/certain.cfg', 'w') as c:
+    with open('setup/etc/certain/certain.cfg', 'w') as c:
         c.write("# Certain's default configuration is given below.\n"
                 "# Uncomment lines as appropriate to change.\n\n")
         with open('certain/certain.cfg.defaults') as f:
@@ -84,7 +83,7 @@ data_files.append(
     (os.path.join('/etc', 'init.d'),
     ['etc/init.d/certain', 'etc/init.d/storeserver']))
 data_files.append(
-    (os.path.join('/etc', 'certain'), ['setup/etc/certain.cfg']))
+    (os.path.join('/etc', 'certain'), ['setup/etc/certain/certain.cfg']))
 
 setup(
     name = 'certain',
