cc8c3df511d5d789d1f705413d52a64c0cf7d4f8
Matthew Richardson <M.Richardson@ed.ac.uk>
2010-04-21 12:09:51 +0100

    Added auto-versioning to setup.py using git describe

--- setup.py
+++ setup.py
@@ -1,10 +1,77 @@
 #!/usr/bin/python
 
+#git describe versioning magic from http://gist.github.com/300803
+
 import sys
 from setuptools import setup, find_packages
 import errno
 import os
 from PyQt4 import uic
+from subprocess import Popen, PIPE
+
+
+def call_git_describe(abbrev=4):
+    try:
+        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
+                  stdout=PIPE, stderr=PIPE)
+        p.stderr.close()
+        line = p.stdout.readlines()[0]
+        return line.strip()
+
+    except:
+        return None
+
+
+def read_release_version():
+    try:
+        f = open("RELEASE-VERSION", "r")
+
+        try:
+            version = f.readlines()[0]
+            return version.strip()
+
+        finally:
+            f.close()
+
+    except:
+        return None
+
+
+def write_release_version(version):
+    f = open("RELEASE-VERSION", "w")
+    f.write("%s\n" % version)
+    f.close()
+
+
+def get_git_version(abbrev=4):
+    # Read in the version that's currently in RELEASE-VERSION.
+
+    release_version = read_release_version()
+
+    # First try to get the current version using `git describe`.
+
+    version = call_git_describe(abbrev)
+
+    # If that doesn't work, fall back on the value that's in
+    # RELEASE-VERSION.
+
+    if version is None:
+        version = release_version
+
+    # If we still don't have anything, that's an error.
+
+    if version is None:
+        raise ValueError("Cannot find the version number!")
+
+    # If the current version is different from what's in the
+    # RELEASE-VERSION file, update the file to be current.
+
+    if version != release_version:
+        write_release_version(version)
+
+    # Finally, return the current version.
+
+    return version
 
 
 if sys.argv[1] == 'install':
@@ -88,7 +155,7 @@ data_files.append(
 
 setup(
     name = 'certain',
-    version = '0.1.0',
+    version = get_git_version(),
     description = 'X509 Certificate Management & Distribution Service',
     author = 'Matthew Richardson',
     author_email = 'm.richardson@ed.ac.uk',
