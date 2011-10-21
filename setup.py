a0fbca2a2d6d9969b56b72e6a7ea7d7767ccdd40
Matthew Richardson <M.Richardson@ed.ac.uk>
2011-10-21 13:55:49 +0100

    Tidying up of setup.py and config naming
    - Create Prebuild extra command in setup.py and call it in builds
    - Change .cfg to .conf for config file names

--- setup.py
+++ setup.py
@@ -4,10 +4,84 @@
 
 import sys
 from setuptools import setup, find_packages
+from distutils.core import setup, Command
 import errno
 import os
 from PyQt4 import uic
 from subprocess import Popen, PIPE
+
+
+if len(sys.argv) > 1 and (
+        not sys.argv[1].startswith('-') and sys.argv[1] != 'clean'):
+    try:
+        with open('certain/CertainForm.py'):
+            pass
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
+        sys.argv.insert(1, 'prebuild')
+        sys.argv.insert(2, 'build_sphinx')
+
+
+class PreBuildCommand(Command):
+    """Custom commands to be run at start of setup.py"""
+
+    description = "Custom commands to be run before main command"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.compile_ui()
+        self.generate_config()
+        self.generate_docs()
+        try:
+            os.makedirs('setup/sphinx')
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    def compile_ui(self):
+        with open('certain/CertainForm.py', 'w') as f:
+            uic.compileUi('certain/certain.ui', f)
+
+    def generate_config(self):
+        try:
+            os.makedirs('setup/etc/certain')
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise
+        #Copy certain.conf.defaults to certain.conf
+        with open('setup/etc/certain/certain.conf', 'w') as c:
+            c.write("# Certain's default configuration is given below.\n"
+                    "# Uncomment lines as appropriate to change.\n\n")
+            with open('certain/certain.conf.defaults') as f:
+                for line in f:
+                    if line.isspace() or line.startswith('['):
+                        c.write(line)
+                    else:
+                        c.write("#" + line)
+
+    def generate_docs(self):
+        #Build reSt config options from defaults
+        comment = []
+        with open('sphinx/config.rst', 'w') as rst:
+            with open('certain/certain.conf.defaults') as cfg:
+                for line in cfg:
+                    if line.startswith('['):
+                        rst.write(line + "------------------------------\n\n")
+                    elif line.startswith('# note: '):
+                        rst.write('\t.. note:: ' + line[8:])
+                    elif line.startswith('#'):
+                        comment += ['\n\t\t' + line[1:]]
+                    else:
+                        rst.write("\t" + line + ''.join(comment))
+                        comment = []
 
 
 def call_git_describe(abbrev=4):
@@ -59,47 +133,7 @@ def get_git_version(abbrev=4):
     return version
 
 
-if sys.argv[1] == 'install':
-    #Build reSt config options from defaults
-    comment = []
-    with open('sphinx/config.rst', 'w') as rst:
-        with open('certain/certain.cfg.defaults') as cfg:
-            for line in cfg:
-                if line.startswith('['):
-                    rst.write(line + "------------------------------\n\n")
-                elif line.startswith('# note: '):
-                    rst.write('\t.. note:: ' + line[8:])
-                elif line.startswith('#'):
-                    comment += ['\n\t\t' + line[1:]]
-                else:
-                    rst.write("\t" + line + ''.join(comment))
-                    comment = []
-
-    #Build sphinx documentation
-    os.system('sphinx-build -b html sphinx ' + os.path.join('setup', 'sphinx'))
-
-    try:
-        os.makedirs('setup/etc/certain')
-    except OSError, e:
-        if e.errno != errno.EEXIST:
-            raise
-
-    #Copy certain.cfg.defaults to certain.cfg
-    with open('setup/etc/certain/certain.cfg', 'w') as c:
-        c.write("# Certain's default configuration is given below.\n"
-                "# Uncomment lines as appropriate to change.\n\n")
-        with open('certain/certain.cfg.defaults') as f:
-            for line in f:
-                if line.isspace() or line.startswith('['):
-                    c.write(line)
-                else:
-                    c.write("#" + line)
-
-elif sys.argv[1] == 'build':
-    with open('certain/CertainForm.py', 'w') as f:
-        uic.compileUi('certain/certain.ui', f)
-
-elif sys.argv[1] == 'clean':
+if len(sys.argv) > 1 and sys.argv[1] == 'clean':
     for dir in ['setup', 'certain.egg-info']:
         try:
             for dirpath, dirs, files in os.walk(dir, topdown=False):
@@ -126,7 +160,7 @@ for (dirpath, dirs, files) in os.walk(os.path.join('setup', 'sphinx')):
     #strip off the initial 'sphinx/build/' for tgt path creation
     subdir = dirpath[13:]
     sphinxfiles.append(
-        (os.path.join('share', 'doc', 'certain', 'html', subdir),
+        (os.path.join('share', 'doc', 'certain', subdir),
          [os.path.join(dirpath, name) for name in files]))
 
 #Add data files to array
@@ -135,7 +169,10 @@ data_files.append(
     (os.path.join('/etc', 'init.d'),
     ['etc/init.d/certain', 'etc/init.d/storeserver']))
 data_files.append(
-    (os.path.join('/etc', 'certain'), ['setup/etc/certain/certain.cfg']))
+    (os.path.join('/etc', 'certain'), ['setup/etc/certain/certain.conf']))
+
+cmdclass = {}
+cmdclass['prebuild'] = PreBuildCommand
 
 setup(
     name = 'certain',
@@ -143,11 +180,12 @@ setup(
     description = 'X509 Certificate Management & Distribution Service',
     author = 'Matthew Richardson',
     author_email = 'm.richardson@ed.ac.uk',
-    url = 'http://www.example.com/certain',
+    url = 'https://github.com/certain/certain',
     packages = find_packages(),
     include_package_data = True,
-    package_data = {'': ['certain.cfg.defaults', 'RELEASE-VERSION']},
+    package_data = {'': ['certain.conf.defaults', 'RELEASE-VERSION']},
     install_requires = ['dulwich', 'M2Crypto'],
     scripts = ['bin/certain', 'bin/storeserver', 'bin/certaingui'],
-    data_files = data_files
+    data_files = data_files,
+    cmdclass = cmdclass,
     )
