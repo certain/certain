7adb4982142a5f97a7ef9f4edbf2dadda955d7ea
Matthew Richardson <M.Richardson@ed.ac.uk>
2011-10-21 10:12:14 +0100

    First stab at extra setup.py methods

--- setup.py
+++ setup.py
@@ -4,10 +4,85 @@
 
 import sys
 from setuptools import setup, find_packages
+from distutils.core import setup, Command
 import errno
 import os
+import os.path
 from PyQt4 import uic
 from subprocess import Popen, PIPE
+
+
+if not sys.argv[1].startswith('-'):
+    if sys.argv[1] not in ('clean'):
+        if not os.path.exists('setup'):
+            sys.argv.insert(1, 'prebuild')
+            sys.argv.insert(2, 'build_sphinx')
+
+
+class CustomCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+class PreBuildCommand(CustomCommand):
+    """Custom commands to be run at start of setup.py"""
+
+    description = "Custom commands to be run before main command"
+
+    def run(self):
+        compile_ui()
+        generate_config()
+        generate_docs()
+        try:
+            os.makedirs('setup/sphinx')
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+def compile_ui():
+    with open('certain/CertainForm.py', 'w') as f:
+        uic.compileUi('certain/certain.ui', f)
+
+
+def generate_config():
+    try:
+        os.makedirs('setup/etc/certain')
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
+    #Copy certain.cfg.defaults to certain.cfg
+    with open('setup/etc/certain/certain.cfg', 'w') as c:
+        c.write("# Certain's default configuration is given below.\n"
+                "# Uncomment lines as appropriate to change.\n\n")
+        with open('certain/certain.cfg.defaults') as f:
+            for line in f:
+                if line.isspace() or line.startswith('['):
+                    c.write(line)
+                else:
+                    c.write("#" + line)
+
+def generate_docs():
+    #Build reSt config options from defaults
+    comment = []
+    with open('sphinx/config.rst', 'w') as rst:
+        with open('certain/certain.cfg.defaults') as cfg:
+            for line in cfg:
+                if line.startswith('['):
+                    rst.write(line + "------------------------------\n\n")
+                elif line.startswith('# note: '):
+                    rst.write('\t.. note:: ' + line[8:])
+                elif line.startswith('#'):
+                    comment += ['\n\t\t' + line[1:]]
+                else:
+                    rst.write("\t" + line + ''.join(comment))
+                    comment = []
 
 
 def call_git_describe(abbrev=4):
@@ -59,47 +134,7 @@ def get_git_version(abbrev=4):
     return version
 
 
-if sys.argv[1] == 'install':
-    #Build reSt config options from defaults
-    comment = []
-    with open('sphinx/config.rst', 'w') as rst:
-        with open('certain/certain.cfg.defaults') as cfg:
-            for line in cfg:
-                if line.startswith('['):
-                    rst.write(line + "------------------------------\n\n")
-                elif line.startswith('# note: '):
-                    rst.write('\t.. note:: ' + line[8:])
-                elif line.startswith('#'):
-                    comment += ['\n\t\t' + line[1:]]
-                else:
-                    rst.write("\t" + line + ''.join(comment))
-                    comment = []
-
-    #Build sphinx documentation
-    os.system('sphinx-build -b html sphinx ' + os.path.join('setup', 'sphinx'))
-
-    try:
-        os.makedirs('setup/etc/certain')
-    except OSError, e:
-        if e.errno != errno.EEXIST:
-            raise
-
-    #Copy certain.cfg.defaults to certain.cfg
-    with open('setup/etc/certain/certain.cfg', 'w') as c:
-        c.write("# Certain's default configuration is given below.\n"
-                "# Uncomment lines as appropriate to change.\n\n")
-        with open('certain/certain.cfg.defaults') as f:
-            for line in f:
-                if line.isspace() or line.startswith('['):
-                    c.write(line)
-                else:
-                    c.write("#" + line)
-
-elif sys.argv[1] == 'build':
-    with open('certain/CertainForm.py', 'w') as f:
-        uic.compileUi('certain/certain.ui', f)
-
-elif sys.argv[1] == 'clean':
+if sys.argv[1] == 'clean':
     for dir in ['setup', 'certain.egg-info']:
         try:
             for dirpath, dirs, files in os.walk(dir, topdown=False):
@@ -137,17 +172,22 @@ data_files.append(
 data_files.append(
     (os.path.join('/etc', 'certain'), ['setup/etc/certain/certain.cfg']))
 
+cmdclass = {}
+cmdclass['prebuild'] = PreBuildCommand
+
 setup(
     name = 'certain',
     version = get_git_version(),
     description = 'X509 Certificate Management & Distribution Service',
     author = 'Matthew Richardson',
     author_email = 'm.richardson@ed.ac.uk',
-    url = 'http://www.example.com/certain',
+    url = 'https://github.com/certain/certain',
     packages = find_packages(),
     include_package_data = True,
     package_data = {'': ['certain.cfg.defaults', 'RELEASE-VERSION']},
     install_requires = ['dulwich', 'M2Crypto'],
     scripts = ['bin/certain', 'bin/storeserver', 'bin/certaingui'],
-    data_files = data_files
+    data_files = data_files,
+    cmdclass = cmdclass
     )
+
